416c58ea05d5cad1bcc13ae190c0bbeeaf66eea7
aeryskyB <79935164+aeryskyB@users.noreply.github.com>
2025-04-18 04:19:33 +0530

    fix(config): try changing source url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://aeryskyb.github.io/obsidianXquartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",//"modified",
     theme: {
